6bd3bb06218255a1a1369d316dd41c230ae3cd04
Adarsh Tiwari <a.tiwariadarsh99@gmail.com>
2024-06-16 01:04:22 +0530

    First commit

--- Flask API test/pythonTest.py
+++ Flask API test/pythonTest.py
@@ -1,7 +1,1 @@
-test="Hi I am Adarsh and this is github commit from VSCode................"
-b=12
-print(test)
-
-print(b)
-
-print("This is a test for github commit from VSCode")
+print("Hi I am trying to commit this on github")
