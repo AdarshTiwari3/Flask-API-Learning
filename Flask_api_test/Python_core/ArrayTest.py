3b8ac3b248616a89252a45cf9e89c3ef7dc95a8d
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-06-29 13:41:20 +0530

    array-practice

--- Flask_api_test/Python_core/ArrayTest.py
+++ Flask_api_test/Python_core/ArrayTest.py
@@ -3,6 +3,6 @@ arr = array('i', [1, 2, 3, 4, 5])
 for i in arr:
     print(i)
 print()
-newArray = array(arr.typecode, (a for a in arr)) #takes one-one value at a time from arr and stores it in newArray
+newArray = array(arr.typecode, (a*a*a for a in arr)) #takes one-one value at a time from arr and stores it in newArray
 for i in newArray:
     print(i)
