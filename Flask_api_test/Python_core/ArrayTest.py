6604fd3a8bd4836398fe208d267f3414e7eca786
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-06-29 13:38:20 +0530

    array-practice

--- Flask_api_test/Python_core/ArrayTest.py
+++ Flask_api_test/Python_core/ArrayTest.py
@@ -2,3 +2,7 @@ from array import *
 arr = array('i', [1, 2, 3, 4, 5])
 for i in arr:
     print(i)
+print()
+newArray = array(arr.typecode, (a for a in arr)) #takes one-one value at a time from arr and stores it in newArray
+for i in newArray:
+    print(i)
