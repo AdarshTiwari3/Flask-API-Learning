f6057ad3c4962477c21745530bc3fcdf3617b685
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-18 23:12:35 +0530

    numpy-dimension-array-practice

--- Flask_api_test/Python_core/dimension_array.py
+++ Flask_api_test/Python_core/dimension_array.py
@@ -13,3 +13,7 @@ print("size of array=", arr.size)
 print("type of array=", arr.dtype)
 print("itemsize of array=", arr.itemsize) # size of each element in bytes
 print("nbytes of array=", arr.nbytes) # total size of array in bytes    8x9=72
+arr2=arr.flatten() # convert 2D array to 1D array
+arr3=arr2.reshape(3,3) # convert 1D array to 2D array
+print("flattened array=", arr2)
+print("reshaped array=", arr3)
