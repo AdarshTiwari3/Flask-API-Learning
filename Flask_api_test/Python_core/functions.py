96aa49b16d7447d7d100bea192044ce808f8ef9f
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-20 14:20:31 +0530

    global-local-variable-in-scope

--- Flask_api_test/Python_core/functions.py
+++ Flask_api_test/Python_core/functions.py
@@ -16,9 +16,9 @@ print(swap_two_numbers(1, 2))
 def swap_two_numbers(a, b):
     a, b=b, a
     return a, b
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-print(swap_two_numbers(a, b))
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+print(swap_two_numbers(5, 6))
 
 # function to take variable length arguments
 def sum(*varLen):
@@ -26,7 +26,7 @@ def sum(*varLen):
     for i in varLen:
         s=s+i
     return s
-print(sum(a, b, 3, 4, 5)) # takes as a tuple
+print(sum(9, 6, 3, 4, 5)) # takes as a tuple
 
 # function to take keyword variable length keyword arguments
 
@@ -37,3 +37,17 @@ def display(name, **varLen):
 
 # display(name="Adarsh", age=20, city="New Delhi", country="India") # takes as a dictionary
 display( age=20, city="New Delhi", country="India", name="Adarsh") #both are same
+
+#global and local variables
+
+globalVar=11
+
+def func():
+    localVar=10
+    global globalVar
+    globalVar=12 # this behaves like local variable to change the value of global variable inside the function scope use global keyword
+    print("localVar=", localVar)
+    print("globalVar=", globalVar)
+
+func()
+print("globalVar=", globalVar)
