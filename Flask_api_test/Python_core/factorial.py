930e9b52418161ff5a4acb46bc63ea6fd32f5551
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-21 15:19:29 +0530

    map-filter-reduce-explained

--- Flask_api_test/Python_core/factorial.py
+++ Flask_api_test/Python_core/factorial.py
@@ -52,3 +52,30 @@ if(n>0):
     print("Factorial of {} using reduce function is: {}".format(n, fact))
 else:
     print("Please enter a non-negative integer.")
+
+#map filter reduce
+#filter function
+lst=[1,2,3,4,8,99,3,5,6,7,9,10]
+x=filter(lambda x: x%2!=0, lst) #filter function takes two arguments, one is the function and the other is the iterable
+print(list(x))
+
+#map function
+x=map(lambda x: x*x, lst) #map function takes two arguments, one is the function and the other is the iterable
+print(list(x)) #map function returns a map object, so we need to convert it into list, [1, 4, 9, 16, 64, 9801, 9, 25, 36, 49, 81, 100]
+
+#reduce function
+
+x=reduce(lambda x, y: x+y, lst) #reduce function takes two arguments, one is the function and the other is the iterable
+print(x) #reduce function returns a single value, 157
+#working-
+#1+2=3 x+y
+#3+3=6 x+y, x gets total value and y gets list values one by one
+#6+4=10
+#10+8=18
+#18+99=117
+#117+3=120
+#120+5=125
+#125+6=131
+#131+7=138
+#138+9=147
+#147+10=157
