cf35db2c49e5cce74dfaa776e6e895a194905139
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-24 00:14:57 +0530

    class/static/instance method

--- Flask_api_test/Python_core/OOPs/classExample.py
+++ Flask_api_test/Python_core/OOPs/classExample.py
@@ -17,16 +17,22 @@ class Contact:
         self.name=name  #instance variable, this goes in the instance namespace
         self.phoneNumber=phoneNumber #instance variable
     
-    def getContact(self):
+    def getContact(self): #getter method or accessor method, instance method because it is using instance variables
         print("Name:{} and Contact Number:{}".format(self.name, self.phoneNumber))
 
-    def setContact(self, name, phoneNumber):
+    def setContact(self, name, phoneNumber): #setter method or mutator method, instance method
         self.name=name
         if(phoneNumber.isdigit() and len(phoneNumber)==10):
             self.phoneNumber=phoneNumber
         else:
             print("Incorrect Phone Number Format, Please enter a valid phone number.")
             self.phoneNumber=None
+    @classmethod
+    def isHumancheck(cls): #class method because it is using class variable
+        print("Human:{}".format(cls.isHuman))
+    @staticmethod
+    def info():
+        print("This is a static method.") #static method because it is not using any instance or class variables
 
 contact1=Contact("Adarsh", '9293293292')
 contact1.getContact()
@@ -37,3 +43,5 @@ contact1.getContact()
 print(contact1.isHuman)
 Contact.isHuman=False #changing the class variable
 print(contact1.isHuman)
+Contact.isHumancheck()
+Contact.info()
