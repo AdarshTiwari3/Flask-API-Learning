b1d575acb9ccc5b405c68529aa366046e2dc49dc
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-24 23:30:13 +0530

    Multilevel-Inheritance

--- Flask_api_test/Python_core/OOPs/inheritance.py
+++ Flask_api_test/Python_core/OOPs/inheritance.py
@@ -9,7 +9,18 @@ class B(A): # B is inheriting class A
         print("I am Feature 3")
     def feature4(self):
         print("I am Feature 4")
+
+class C(B): # C is inheriting class B
+    def display(self):
+        print("I am in Class C")
+
+#this is an example of multilevel inheritance
 b1=B()
 b1.feature1()
 b1.feature2()
 b1.feature3()
+c1=C()
+c1.display()
+c1.feature1()
+c1.feature3()
+
