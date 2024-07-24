bd5044ef3057ba7b07130d6a4cc1f42dd4162388
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-07-25 00:19:39 +0530

    Duck-Type-Polymorphism

---
+++ Flask_api_test/Python_core/OOPs/polymorphism.py
@@ -0,0 +1,29 @@
+#Polymorphism: Polymorphism is an ability (in OOP) to use a common interface for multiple forms (data types). use one in many forms
+#There are four ways to implement polymorphism:
+#1. Duck Typing
+#2. Operator Overloading
+#3. Method Overloading
+#4. Method Overriding
+
+#Duck Typing: It is a concept related to dynamic typing, where the type or the class of an object is less important than the methods it defines. 
+#it doesn't matter what type of object it is, it matters what methods it has
+
+class VsCode:
+    def display(self):
+        print("I am inside VsCode")
+        print("Program is executing...")
+
+class Laptop:
+    def code(self, editor):
+        editor.display()
+
+class MyEditor:
+    def display(self):
+        print("I am inside MyEditor")
+        print("Program is executing...")
+
+# editor=VsCode()
+editor=MyEditor()
+lap=Laptop()
+# lap.code(editor)
+lap.code(editor)
