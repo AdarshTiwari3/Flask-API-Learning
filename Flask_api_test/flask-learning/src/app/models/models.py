a6e0479a8f1de13fed1d08c03b9311673f2f3582
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-03 00:29:56 +0530

    db-practice

--- Flask_api_test/flask-learning/src/app/models/models.py
+++ Flask_api_test/flask-learning/src/app/models/models.py
@@ -6,3 +6,15 @@ class User(db.Model):
     username = db.Column(db.String(200), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.password}')"
+class Car(db.Model):
+    __tablename__ = 'car'
+    car_id = db.Column(db.Integer, primary_key=True)
+    model= db.Column(db.String(200), nullable=False)
+    owner = db.Column(db.String(200), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Car('{self.model}', '{self.owner}', '{self.year}')"
